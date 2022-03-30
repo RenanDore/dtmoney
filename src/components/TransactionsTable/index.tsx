04d52fb5801035d71a62250fc157c427ac886cd3
Renan Dore <dore.renan@gmail.com>
2022-03-30 09:35:00 -0300

    criando novos componentes que formam a aplicacao

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,40 @@
+import { useContext } from "react";
+import { TransactionsContext } from "../../TransactionContexts";
+import { Container } from "./styles";
+
+export function TransactionsTable() {
+  const { transactions } = useContext(TransactionsContext);
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Titulo</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {transactions.map((transactions) => (
+            <tr key={transactions.id}>
+              <td>{transactions.title}</td>
+              <td className={transactions.type}>
+                {new Intl.NumberFormat("pt-BR", {
+                  style: "currency",
+                  currency: "BRL",
+                }).format(transactions.amount)}
+              </td>
+              <td>{transactions.category}</td>
+              <td>
+                {new Intl.DateTimeFormat("pt-BR").format(
+                  new Date(transactions.createdAt)
+                )}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </Container>
+  );
+}
